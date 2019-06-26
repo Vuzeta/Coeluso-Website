5aeb329f8dbe402b0d2ff7b20e1c5e98b7a2075a
Vuzeta95 <machnio95@o2.pl>
2019-06-26 20:32:11 +0200

    new future

--- src/js/app.js
+++ src/js/app.js
@@ -1,35 +1,31 @@
-import {
-    changeComment
-} from "./comment.js";
-import {
-    Elements
-} from "./elements.js";
-import {
-    playWave
-} from "./wave.js";
-import {
-    showNav
-} from "./showNavbar.js"
-import jump from './jump.js'
+import { changeComment } from "./comment.js";
+import { Elements } from "./elements.js";
+import { playWave } from "./wave.js";
+import { showNav } from "./showNavbar.js";
+import jump from "jump.js";
 
-import "./../scss/main.scss" //webpack sass
-
+import "./../scss/main.scss"; //webpack sass
 
 changeComment(); //box6 function to show comments
 setInterval(playWave, 220); //box2 wave function
 Elements.copyYear.textContent = new Date().getFullYear(); // copy current year
 
-Elements.hamburger.addEventListener('click', function () {
+Elements.hamburger.addEventListener(
+  "click",
+  function() {
     Elements.hamburger.classList.toggle("is-active");
     showNav(Elements.headerLinks);
-}, false);
+  },
+  false
+);
 Elements.copyYear.textContent = new Date().getFullYear(); // copy current year
 
-[...Elements.navigationLinks].forEach(el => {       //function jump when link on navbar is clicked
-    el.addEventListener('click', function (e) {
-        let link = e.target.getAttribute('href');
-        jump(link, {
-            offset: -150,
-        })
-    })
-})
+[...Elements.navigationLinks].forEach(el => {
+  //function jump when link on navbar is clicked
+  el.addEventListener("click", function(e) {
+    let link = e.target.getAttribute("href");
+    jump(link, {
+      offset: -150
+    });
+  });
+});
